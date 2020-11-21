5da02979f2bf4eeed13c6b3eeb8398ca6437472d
mondspiegelung <mondspiegelung@gmail.com>
2020-11-21 03:45:11 -0500

    Adding floating point conversion code.
    This code is based on https://github.com/night-shift/fpconv, and as
    such, requires those files to be dual licensed under the LGPL and Boost
    licences.

---
+++ fptest.cc
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <limits>
+#include <initializer_list>
+
+#include "util/fp_convert.h"
+
+int main()
+{
+	std::initializer_list<double> values = {
+		16384,
+		3.14159,
+		1e-309,
+		5e-324,
+		2.5e-324,
+		0,
+		-0.0,
+	};
+
+	static_assert(2.5e-324 == 5e-324,
+	              "your compiler is no longer rounding 1/2 the smallest "
+	              "representable subnormal value away from zero");
+
+	for (const auto & v : values)
+	{
+		char result[30];
+		unsigned n = util::fp_convert(v, result);
+
+		result[n] = '\0';
+
+		printf("%20g %20s\n", v, result);
+	}
+
+	return 0;
+}
---
+++ util/fp_convert.h
@@ -0,0 +1,10 @@
+#ifndef UTIL_FP_CONVERT_H
+#define UTIL_FP_CONVERT_H 1
+
+namespace util {
+
+int fp_convert(double d, char * dest);
+
+} // namespace util
+
+#endif // UTIL_FP_CONVERT_H
